261eea044a972905f91958f2ae9c6b35e64b4deb
Chowdhury Ashraf <chowdhury.k.ashraf@gmail.com>
2016-01-22 09:03:33 +0000

    simplification

--- init.el
+++ init.el
@@ -6,12 +6,6 @@
 ;;
 
 ;; load Org-mode from source when the ORG_HOME environment variable is set
-(when (getenv "ORG_HOME")
-  (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
-    (when (file-directory-p org-lisp-dir)
-      (add-to-list 'load-path org-lisp-dir)
-      (require 'org))))
-
 ;; load the starter kit from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
  `(lambda ()
